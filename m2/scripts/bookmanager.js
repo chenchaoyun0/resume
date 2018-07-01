66f0f2652f66d18318c8bb1de5b919e263c19cc5
chenchaoyun <chenchaoyun@megvii.com>
2018-07-01 16:08:20 +0800

    增加静态页面异步请求，获取网站访问统计
    Change-Id: Ifa5ebb4e8997ca85c9cb46c42c9b701fbe3060cd

---
+++ m2/scripts/bookmanager.js
@@ -0,0 +1,42 @@
+$(function() {
+	$.ajax({
+		type : 'POST',
+		url : "http://127.0.0.1:9090/bookmanager/lookResume",
+		contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(GetJsonData()),
+        dataType:'json',
+		async : true,
+		success : function(result) {
+			$("#resumeCount").html(result.resumeCount);
+			$("#totalcount").html(result.totalcount);
+			$("#todayCount").html(result.todayCount);
+			$("#todayVisitorCount").html(result.todayVisitorCount);
+			$("#totalVisitorCount").html(result.totalVisitorCount);
+		},
+		error : function(xhr, ajaxOptions, thrownError) {
+			/*alert("error.");
+			if (xhr.status == 200) {
+				alert(ajaxOptions);
+			} else {
+				alert(xhr.status);
+				alert(thrownError);
+			}*/
+		}
+	});
+	function GetJsonData() {
+		var json = {
+			"appName" : navigator.appName,
+			"appCodeName" : navigator.appCodeName,
+			"platform" : navigator.platform,
+			"appVersion" : navigator.appVersion,
+			"userAgent" : navigator.userAgent,
+			"product" : navigator.product,
+			"cpuClass" : navigator.cpuClass,
+			"oscpu" : navigator.oscpu,
+			"productSub" : navigator.productSub,
+			"userProfile" : navigator.userProfile,
+			"cookieEnabled" : navigator.cookieEnabled
+		};
+		return json;
+	}
+});
